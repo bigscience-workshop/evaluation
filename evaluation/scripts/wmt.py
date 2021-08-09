47ff96ca21eeee69dec29f3932b07f088ccd4466
Jake Tae <jaesungtae@gmail.com>
2021-08-10 05:02:30 +0900

    fix: correct repeated var, add reference

--- evaluation/scripts/wmt.py
+++ evaluation/scripts/wmt.py
@@ -15,27 +15,28 @@ def load_wmt(tokenizer):
 
 
 def main(args):
+    """adapted from https://huggingface.co/transformers/perplexity.html"""
     stride = args.stride
     device = torch.device("cuda")
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     model = AutoModelForCausalLM.from_pretrained(args.model).to(device)
-    input_ids = load_wmt(tokenizer).input_ids.to(device)
+    tokens = load_wmt(tokenizer)
     max_length = model.config.n_positions
     log_likelihoods = []
-    total_length = input_ids.size(1)
+    total_length = tokens.input_ids.size(1)
     for i in tqdm(range(0, total_length, stride)):
         begin_loc = max(i + stride - max_length, 0)
         end_loc = min(i + stride, total_length)
         trg_len = end_loc - i
-        input_ids = input_ids[:, begin_loc:end_loc].to(device)
+        input_ids = tokens.input_ids[:, begin_loc:end_loc].to(device)
         target_ids = input_ids.clone()
         target_ids[:, :-trg_len] = -100
         with torch.no_grad():
             outputs = model(input_ids, labels=target_ids)
             log_likelihood = outputs[0] * trg_len
         log_likelihoods.append(log_likelihood)
-    perplexity = torch.exp(torch.stack(lls).sum() / end_loc)
-    print(perplexity)
+    perplexity = torch.exp(torch.stack(log_likelihoods).sum() / end_loc)
+    print(f"perplexity: {perplexity.item():.3f}")
 
 
 if __name__ == "__main__":
