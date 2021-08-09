f8948080c274b3168a41276e5ff3a77c116e19d9
Wilson Lee <gowaneil@gmail.com>
2021-08-09 12:00:27 -0700

    feat: set random seed in simple_benchmark

--- evaluation/utils/io.py
+++ evaluation/utils/io.py
@@ -3,6 +3,5 @@ from typing import Dict
 
 
 def save_json(content: Dict, path: str, indent: int = 4, **kwargs) -> None:
-    print(path)
     with open(path, "w") as f:
         json.dump(content, f, indent=indent, sort_keys=True, **kwargs)
