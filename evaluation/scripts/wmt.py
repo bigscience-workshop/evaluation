5b6f8f0d15362240414d21a32074f369fea0175e
Jake Tae <jaesungtae@gmail.com>
2021-08-10 12:56:44 +0900

    fix: decouple tokenizer fro `load_wmt()`

--- evaluation/scripts/wmt.py
+++ evaluation/scripts/wmt.py
@@ -6,12 +6,12 @@ from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-def load_wmt(tokenizer):
+def load_wmt():
     wmt19 = load_dataset("wmt19", "kk-en")
     validation_set = wmt19["validation"]["translation"]
     text_list = [item["en"] for item in validation_set]
     text = " ".join(text_list)
-    return tokenizer(text, return_tensors="pt")
+    return text
 
 
 def main(args):
@@ -20,10 +20,11 @@ def main(args):
     device = torch.device("cuda")
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     model = AutoModelForCausalLM.from_pretrained(args.model).to(device)
-    tokens = load_wmt(tokenizer)
     max_length = model.config.n_positions
-    log_likelihoods = []
+    text = load_wmt()
+    tokens = tokenizer(text, return_tensors="pt")
     total_length = tokens.input_ids.size(1)
+    log_likelihoods = []
     for i in tqdm(range(0, total_length, stride)):
         begin_loc = max(i + stride - max_length, 0)
         end_loc = min(i + stride, total_length)
@@ -43,5 +44,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="gpt2")
     parser.add_argument("--stride", type=int, default=512)
+    parser.add_argument("--batch", type=int, default=-1)
     args = parser.parse_args()
     main(args)
