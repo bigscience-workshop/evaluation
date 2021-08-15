f259b2565646f524693770fb731b7699a38615ef
Jake Tae <jaesungtae@gmail.com>
2021-08-15 21:07:11 +0900

    style: run formatter

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -1,15 +1,11 @@
+import os
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Optional, List
-import os
+from typing import List, Optional
 
 import torch
-from transformers import (
-    HfArgumentParser,
-    AutoTokenizer,
-    AutoModelForCausalLM,
-    set_seed,
-)
+from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser, set_seed
+
 import evaluation.tasks  # needed for AutoTask.__subclass__() to work correctly
 from evaluation.tasks.auto_task import AutoTask
 from evaluation.utils.log import get_logger
@@ -18,46 +14,49 @@ from evaluation.utils.log import get_logger
 @dataclass
 class EvaluationArguments:
     """
-        Arguments for any adjustable params in this evaluation script
+    Arguments for any adjustable params in this evaluation script
     """
+
     model_name_or_path: Optional[str] = field(
         default=None,
-        metadata={"help": "The model checkpoint that we want to evaluate, could be name or the path."}
+        metadata={
+            "help": "The model checkpoint that we want to evaluate, could be name or the path."
+        },
     )
     config_name: Optional[str] = field(
         default=None,
-        metadata={"help": "Pretrained config name or path if not the same as model_name."}
+        metadata={
+            "help": "Pretrained config name or path if not the same as model_name."
+        },
     )
     tokenizer_name: Optional[str] = field(
         default=None,
-        metadata={"help": "Pretrained tokenizer name or path if not the same as model_name."}
+        metadata={
+            "help": "Pretrained tokenizer name or path if not the same as model_name."
+        },
     )
     output_dir: str = field(
-        default="outputs",
-        metadata={"help": "Directory for saving evaluation outputs."}
+        default="outputs", metadata={"help": "Directory for saving evaluation outputs."}
     )
     tag: Optional[str] = field(
-        default=None,
-        metadata={"help": "Identifier for the evaluation run."}
+        default=None, metadata={"help": "Identifier for the evaluation run."}
     )
-    random_seed: int = field(
-        default=24,
-        metadata={"help": "Customized random seed"}
-    )
+    random_seed: int = field(default=24, metadata={"help": "Customized random seed"})
     eval_tasks: Optional[List[str]] = field(
         default=None,
-        metadata={"help": "A list of tasks to run the evaluation on, e.g. tydiqa_secondary"}
+        metadata={
+            "help": "A list of tasks to run the evaluation on, e.g. tydiqa_secondary"
+        },
     )
     device: str = field(
-        default="cuda",
-        metadata={"help": "Device on which to run evaluation"}
+        default="cuda", metadata={"help": "Device on which to run evaluation"}
     )
 
 
 def main(args):
     if not args.eval_tasks:
-        raise ValueError('Must provide at least one eval task!')
-    
+        raise ValueError("Must provide at least one eval task!")
+
     # initialize device
     device = torch.device(args.device)
 
@@ -66,11 +65,15 @@ def main(args):
 
     # Load model & tokenizer
     logger.info("Loading model...")
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name or args.model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.tokenizer_name or args.model_name_or_path
+    )
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "left"
 
-    model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, pad_token_id=tokenizer.eos_token)
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_name_or_path, pad_token_id=tokenizer.eos_token
+    )
     model.config.pad_token_id = model.config.eos_token_id
     model.resize_token_embeddings(len(tokenizer))
     model.to(device)
@@ -82,7 +85,9 @@ def main(args):
 
     for eval_task in args.eval_tasks:
         logger.info(f"Benchmarking {eval_task}...")
-        task = AutoTask.from_task_name(eval_task, tokenizer=tokenizer, model=model, device=device)
+        task = AutoTask.from_task_name(
+            eval_task, tokenizer=tokenizer, model=model, device=device
+        )
         set_seed(args.random_seed)
         task.evaluate()
         task.save_metrics(output_dir, logger)
@@ -90,5 +95,5 @@ def main(args):
 
 if __name__ == "__main__":
     parser = HfArgumentParser(EvaluationArguments)
-    args, = parser.parse_args_into_dataclasses()
+    (args,) = parser.parse_args_into_dataclasses()
     main(args)
--- evaluation/tasks/auto_task.py
+++ evaluation/tasks/auto_task.py
@@ -1,5 +1,5 @@
-from abc import ABC, abstractmethod
 import os
+from abc import ABC, abstractmethod
 
 from evaluation.utils.io import save_json
 
@@ -17,8 +17,8 @@ class AutoTask(ABC):
         for task in all_tasks:
             if task.get_display_name() == task_name:
                 return task(tokenizer=tokenizer, model=model, device=device)
-        
-        raise ValueError(f'Invalid task: {task_name}')
+
+        raise ValueError(f"Invalid task: {task_name}")
 
     @staticmethod
     @abstractmethod
@@ -34,5 +34,7 @@ class AutoTask(ABC):
         save_json(self.metrics, output_filename)
 
         if logger:
-            logger.info(f"{self.get_display_name()}: result exported to {output_filename}")
+            logger.info(
+                f"{self.get_display_name()}: result exported to {output_filename}"
+            )
         return output_filename
