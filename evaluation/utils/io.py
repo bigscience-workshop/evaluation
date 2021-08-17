bb958d5bc0524a53375f1fd88f1985d5e2a9e6f9
Jake Tae <jaesungtae@gmail.com>
2021-08-18 00:21:41 +0900

    feat: implement `load_json()`

--- evaluation/utils/io.py
+++ evaluation/utils/io.py
@@ -5,3 +5,7 @@ from typing import Dict
 def save_json(content: Dict, path: str, indent: int = 4, **kwargs) -> None:
     with open(path, "w") as f:
         json.dump(content, f, indent=indent, sort_keys=True, **kwargs)
+
+def load_json(path: str) -> Dict:
+    with open(path) as f:
+        return json.load(f)
