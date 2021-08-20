8aed4b03af3d7287a4c7862433fb6b57f577b14d
tttyuntian <tttyuntian@hotmail.com>
2021-08-19 20:24:09 -0400

    style: run formatter

--- evaluation/tasks/piqa/piqa.py
+++ evaluation/tasks/piqa/piqa.py
@@ -1,11 +1,10 @@
 # Module for any additional processing required for the TyDi QA dataset
 # HuggingFace dataset link: https://huggingface.co/datasets/piqa
 from datasets import load_dataset
+from evaluation.tasks.auto_task import AutoTask
 from jinja2 import Template
 from torch.utils.data import Dataset
 from tqdm import tqdm
-
-from evaluation.tasks.auto_task import AutoTask
 
 TEMPLATE = Template(
     """
@@ -26,15 +25,14 @@ class PIQADataset(Dataset):
 
         for sample in piqa:
             prompt = TEMPLATE.render(
-                goal = sample["goal"],
-                sol1 = sample["sol1"],
-                sol2 = sample["sol2"],
+                goal=sample["goal"],
+                sol1=sample["sol1"],
+                sol2=sample["sol2"],
             )
 
             # Tokenize and construct this sample
             inputs = tokenizer(
                 prompt,
-                #padding=True,
                 return_tensors="pt",
             )
             self.items.append(
@@ -46,28 +44,28 @@ class PIQADataset(Dataset):
                     "label": [sample["sol1"], sample["sol2"]][sample["label"]],
                 }
             )
-    
+
     def __len__(self):
         return len(self.items)
-    
+
     def __getitem__(self, index):
         return self.items[index]
-    
-    
+
+
 class PIQATask(AutoTask):
     @staticmethod
     def get_display_name() -> str:
         return "piqa"
-    
+
     def evaluate(self) -> None:
         dataset = PIQADataset(self.tokenizer)
-        
+
         substring_matches = 0
         for sample in tqdm(dataset):
             output = self.model.generate(
                 input_ids=sample["input_ids"].to(self.device),
                 attention_mask=sample["attention_mask"].to(self.device),
-                max_length=min(sample["input_len"]*2, self.model.config.n_positions),
+                max_length=min(sample["input_len"] * 2, self.model.config.n_positions),
             )
             prompt_len = len(sample["prompt"])
             decoded_output = self.tokenizer.decode(output[0], skip_special_tokens=True)
@@ -77,7 +75,7 @@ class PIQATask(AutoTask):
             substring_match = int(label.lower() in predicted_answer.lower())
 
             substring_matches += substring_match
-        
+
         self.metrics = {
             "substring_match": substring_matches / len(dataset) * 100,
         }
