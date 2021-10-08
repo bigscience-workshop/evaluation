838cc3618a984dedebc1b895bcc4e8a647b1f553
JanKalo <j.c.kalo@vu.nl>
2021-10-08 18:17:14 +0200

    fix: formatting style

--- evaluation/tasks/lama_trex/lama_trex.py
+++ evaluation/tasks/lama_trex/lama_trex.py
@@ -1,40 +1,35 @@
+from datasets import load_dataset
 from torch.utils.data import Dataset
 from tqdm import tqdm
-from datasets import load_dataset
+
 from evaluation.tasks.auto_task import AutoTask
-
-
 
 
 class LAMA_Trex_Dataset(Dataset):
     def __init__(self, tokenizer):
         super().__init__()
-        #load trex dataset
-        lama = load_dataset("lama","trex",split="train")
+        # load trex dataset
+        lama = load_dataset("lama", "trex", split="train")
 
         self.items = []
 
         triples_added = set()
         for sample_id, sample in enumerate(lama):
-            id=sample["uuid"],
-            obj_label=sample["obj_label"],
-            sub_label=sample["sub_label"]
+            id = (sample["uuid"],)
+            obj_label = (sample["obj_label"],)
+            sub_label = sample["sub_label"]
             template = sample["template"]
             predicate_id = sample["predicate_id"]
             template = template.strip()  # Remove trailing white space and newline
 
             # adapt the [MASK ]template to work with a causal LM
             # we cut off the remaining part of the template. this may cause problems for some LAMA templates
-            template = template.replace("[X]",sub_label)
+            template = template.replace("[X]", sub_label)
             template = template.split("[Y]")[0]
             triple = (sub_label, predicate_id, obj_label)
 
-                # Tokenize and construct this sample
-            inputs = tokenizer(
-                template,
-                padding=True,
-                return_tensors="pt",
-            )
+            # Tokenize and construct this sample
+            inputs = tokenizer(template, padding=True, return_tensors="pt",)
             if triple not in triples_added:
                 triples_added.add(triple)
                 self.items.append(
@@ -76,13 +71,12 @@ class LAMA_Trex_Task(AutoTask):
             )
             prompt_len = len(sample["template"])
             decoded_output = self.tokenizer.decode(output[0], skip_special_tokens=True)
-            prediction = decoded_output.split(".")[0].replace(sample["template"],"")
+            prediction = decoded_output.split(".")[0].replace(sample["template"], "")
 
             target_answer = sample["target_answer"]
 
-            #this step is kind of different from the original LAMA evaluation, since it checks whether the correct answer is within a number of predicted words.
+            # this step is kind of different from the original LAMA evaluation, since it checks whether the correct answer is within a number of predicted words.
             if target_answer in prediction:
                 counter += 1
-
 
         self.metrics["precision@1"] = counter / len(dataset)
