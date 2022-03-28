28d2b72a9e344d0be94734aaaf4d43891c761818
Wilson Lee <gowaneil@gmail.com>
2022-03-28 08:11:50 -0700

    style: reformatting code

--- social-impact-group/BiasShades/example_bias_shades_prompt_generation_code.py
+++ social-impact-group/BiasShades/example_bias_shades_prompt_generation_code.py
@@ -1,26 +1,40 @@
-import sys
 import re
+import sys
 
 
 # how fine-grained should the categories be?  CLOTHING_HEAD vs CLOTHING
 # Age+gender vs just age or just gender? Can be both.
-GENDER_PL = ['men','women','boys','girls','non-binary people','gender-fluid']
-GENDERAGE_PL = ['boys','girls','men','women']
-GENDER_POSS = ['his','her','their','xe','ze','sie','co','ey']
-HAIRCOLOR_PL = ['blondes','brunettes','redheads']#'people with blonde hair',
-# 'people \
- #       with brown hair', 'people with red hair', 'people with black hair']
-HAIRCOLOR = ['blonde','brunette','redhead']
-CLOTHING_HEAD_PL = ['glasses','bow-ties','ties','scarves','hats']
-AGE_PL = ['kids','children','seniors','adults']
-BODY_PL = ['fat people','skinny people','muscular people', 'thin people',
-           'larger people','tall people','short people','stout people']
-NATIONALITY_PL = ['the Japanese','the English', "Germans","Romanians"]
-phrases = {'AGE+GENDER-PL':GENDERAGE_PL,'GENDER-PL':GENDER_PL,
-           'GENDER-POSS':GENDER_POSS,
-           'BODY-HAIRCOLOR-PL':HAIRCOLOR_PL,'HAIRCOLOR-MOD':HAIRCOLOR,
-           'CLOTHING-HEAD-PL':CLOTHING_HEAD_PL,
-           'AGE-PL':AGE_PL,'BODY-PL':BODY_PL,'NATIONALITY-PL':NATIONALITY_PL}
+GENDER_PL = ["men", "women", "boys", "girls", "non-binary people", "gender-fluid"]
+GENDERAGE_PL = ["boys", "girls", "men", "women"]
+GENDER_POSS = ["his", "her", "their", "xe", "ze", "sie", "co", "ey"]
+# 'people with blonde hair', 'people with brown hair', 'people with red hair', 'people with black hair'
+HAIRCOLOR_PL = ["blondes", "brunettes", "redheads"]
+HAIRCOLOR = ["blonde", "brunette", "redhead"]
+CLOTHING_HEAD_PL = ["glasses", "bow-ties", "ties", "scarves", "hats"]
+AGE_PL = ["kids", "children", "seniors", "adults"]
+BODY_PL = [
+    "fat people",
+    "skinny people",
+    "muscular people",
+    "thin people",
+    "larger people",
+    "tall people",
+    "short people",
+    "stout people",
+]
+NATIONALITY_PL = ["the Japanese", "the English", "Germans", "Romanians"]
+phrases = {
+    "AGE+GENDER-PL": GENDERAGE_PL,
+    "GENDER-PL": GENDER_PL,
+    "GENDER-POSS": GENDER_POSS,
+    "BODY-HAIRCOLOR-PL": HAIRCOLOR_PL,
+    "HAIRCOLOR-MOD": HAIRCOLOR,
+    "CLOTHING-HEAD-PL": CLOTHING_HEAD_PL,
+    "AGE-PL": AGE_PL,
+    "BODY-PL": BODY_PL,
+    "NATIONALITY-PL": NATIONALITY_PL,
+}
+
 
 def make_eval(input_text):
     for line in input_text:
@@ -32,6 +46,6 @@ def make_eval(input_text):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
     open_file = open(sys.argv[1], "r+").readlines()
     make_eval(open_file)
